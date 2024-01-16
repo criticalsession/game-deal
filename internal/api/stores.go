805e4322452013b025fe53bb8a5df4a319df87aa
criticalsession <criticalsession@protonmail.ch>
2024-01-16 04:49:16 +0100

    add: return stores as map for easy access

--- internal/api/stores.go
+++ internal/api/stores.go
@@ -1,17 +1,29 @@
 package api
 
 import (
+	"strconv"
+
 	"github.com/criticalsession/game-deal/internal/types/stores"
 )
 
-func (c *Config) GetStores() (stores.Resp, error) {
+func (c *Config) GetStores() (map[int]stores.Store, error) {
 	url := baseUrl + "/stores"
 	storeResp := stores.Resp{}
 
 	err := getData(url, c.client, &storeResp)
 	if err != nil {
-		return stores.Resp{}, err
+		return map[int]stores.Store{}, err
 	}
 
-	return stores.Resp(storeResp), nil
+	result := make(map[int]stores.Store)
+	for _, store := range storeResp {
+		id, err := strconv.Atoi(store.StoreID)
+		if err != nil {
+			return map[int]stores.Store{}, err
+		}
+
+		result[id] = store
+	}
+
+	return result, nil
 }
